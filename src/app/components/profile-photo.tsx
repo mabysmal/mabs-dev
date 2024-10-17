56c0779f9861f0be701f3dd5d3ec873fa917522c
Mabel Castillo <maby.garza@gmail.com>
2024-10-17 13:59:19 -0700

    1

--- src/app/components/profile-photo.tsx
+++ src/app/components/profile-photo.tsx
@@ -7,11 +7,12 @@ type TextBox = {
 };
 
 interface CircularProfileProps {
-  speed: number; // Nueva prop para controlar la velocidad
+  speed: number; 
 }
 
 const CircularProfile: React.FC<CircularProfileProps> = ({ speed }) => {
   const [rotation, setRotation] = useState(0);
+  const [isMobile, setIsMobile] = useState(true);
   const textBoxes: TextBox[] = [
     { text: 'programmer', angle: 0 },
     { text: 'photographer', angle: 120 },
@@ -26,22 +27,37 @@ const CircularProfile: React.FC<CircularProfileProps> = ({ speed }) => {
     return () => clearInterval(interval);
   }, [speed]);
 
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 768px)');
+    const handleMediaQueryChange = (e: MediaQueryListEvent) => {
+      setIsMobile(!e.matches);
+    };
+
+    setIsMobile(!mediaQuery.matches);
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
+
   const calculatePosition = (angle: number) => {
     const radian = ((angle + rotation) * Math.PI) / 180;
+    const translateX = isMobile ? 120 : 150;
     return {
-      transform: `rotate(${radian}rad) translateX(150px) rotate(-${radian}rad)`,
+      transform: `rotate(${radian}rad) translateX(${translateX}px) rotate(-${radian}rad)`,
     };
   };
 
   return (
-    <div className="relative w-[300px] h-[300px] flex items-center justify-center md:w-2/3">
-      <div className="absolute w-full h-full  flex items-center justify-center">
+    <div className="relative w-[250px] h-[250px] md:w-[350px] md:h-[350px] flex items-center justify-center">
+      <div className="absolute w-full h-full flex items-center justify-center">
         <Image
           src="/images/iox2.jpg"
           alt="ProfileImage"
           width={300}
           height={300}
-          className="rounded-full "
+          className="rounded-full"
         />
       </div>
       {textBoxes.map((box, index) => (
