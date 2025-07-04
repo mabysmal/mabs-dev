649b9e20a46460fbda9dc19f053a1c962d697b4d
Mabel Castillo <maby.garza@gmail.com>
2025-07-04 12:22:34 -0700

    google analytics added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,35 @@
 import React from "react";
-import HomeClient from "./components/home-client"; // Importamos el componente cliente
+import Script from 'next/script';
+import HomeClient from "./components/home-client";
 import './globals.css';
-
 
 export default function Home() {
   return (
-    <main className=''>
+    <>
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WH335LFS');
+        `}
+      </Script>
       
-     
-      <HomeClient />
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WH335LFS"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
 
-    </main>
+      <main className=''>
+        <HomeClient />
+      </main>
+    </>
   );
 }
